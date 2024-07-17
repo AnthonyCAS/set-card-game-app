c17dc64703fafc9de6be5a203cddbc08636cbfcd
AnthonyCAS <amtony510@gmail.com>
2024-07-17 09:51:34 -0500

    set up initial iOS project

---
+++ SetCardGame/SetCardGame/SetCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetCardGameApp.swift
+//  SetCardGame
+//
+//  Created by zhira on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SetCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
