b1e39c782f1c1328fea52b58b3a0cff6c6d633a7
AnthonyCAS <amtony510@gmail.com>
2024-09-11 14:28:22 -0500

    implement unit tests for the SetGameModel

--- SetCardGame/SetCardGameTests/SetGameModelTests.swift
+++ SetCardGame/SetCardGameTests/SetGameModelTests.swift
@@ -19,10 +19,57 @@ final class SetGameModelTests: XCTestCase {
     }
 
     override func tearDown() {
-        
+        sut = nil
+        scoreTracker = nil
     }
 
-    func testExample() {
+    func testCards_WhenGameStart_Return81CardsAnd0Score() {
+        // Arrange
+        let initialDeckCards = 81
         
+        // Act
+        
+        // Assert
+        XCTAssertEqual(sut.cards.count, initialDeckCards, "The initial cards in the game must be 81")
+        let score = scoreTracker.getScore()
+        XCTAssertEqual(sut.delegate?.getScore(), score, "The score must be Zero")
     }
+    
+    func testCards_WhenChoose3RandomCards_ReturnsZeroMatchedCards() {
+        // Arrange
+        let first = sut.cards.randomElement()
+        let second = sut.cards.randomElement()
+        let third = sut.cards.randomElement()
+        
+        // Act
+        if let first, let second, let third {
+            sut.choose(first)
+            sut.choose(second)
+            sut.choose(third)
+            // Assert
+            let matchedCards = sut.cards.filter { $0.isMatched }
+            XCTAssertEqual(matchedCards.count, 0)
+        } else {
+            XCTFail("Selecting cards failed")
+        }
+    }
+    
+//    func testCards_WhenChoose3RightCards_ReturnsAMatch() {
+//        // Arrange
+//        let first = sut.cards.randomElement()
+//        let second = sut.cards.randomElement()
+//        let third = sut.cards.randomElement()
+//        
+//        // Act
+//        if let first, let second, let third {
+//            sut.choose(first)
+//            sut.choose(second)
+//            sut.choose(third)
+//            // Assert
+//            let matchedCards = sut.cards.filter { $0.isMatched }
+//            XCTAssertEqual(matchedCards.count, 0)
+//        } else {
+//            XCTFail("Selecting cards failed")
+//        }
+//    }
 }
